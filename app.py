f9c3c73e1ce934e61e13eea9373486d644c771a6
santhoshdasi <119122877+santhoshdasi@users.noreply.github.com>
2023-10-30 21:20:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,5 @@ model = pickle.load(open("C:/Users/Dasi Rajesh/OneDrive/Desktop/Flask/templates/
 def Home():
     return render_template("index.html")
 
-
-@flask_app.route("/predict", methods = ["POST"])
-def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
-    return render_template("predict.html", prediction_text = "Average Rating is".format(prediction))
-
 if __name__ == "__main__":
     flask_app.run(debug=True)
